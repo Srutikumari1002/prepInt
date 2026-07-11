119e29c22072cff4df3a2f8c7ba74fcfcee76714
Srutikumari1002 <Srutikumari1002@gmail.com>
2026-07-11 15:54:08 +0530

    feat: complete InterviewAce app and setup Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Change '/prepInt/' to '/<your-new-repository-name>/' for GitHub Pages to work
   base: '/prepInt/',
   plugins: [react()],
 })
